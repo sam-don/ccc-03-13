0b94230edb72aa88282281c36e5e936dc436ff49
Sam Don <71539578+sam-don@users.noreply.github.com>
2020-11-13 12:05:43 +1100

    User auth added

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,9 +1,25 @@
-from flask import Blueprint
-from schemas.UserSchema import user_schema
+from flask import Blueprint, request, abort, jsonify
+from schemas.UserSchema import UserSchema, user_schema
 from models.User import User
+from main import db
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth.route("/register", methods=["POST"])
 def auth_register():
-    return "Still Working"
+    user_fields = user_schema.load(request.json)
+
+    user = User.query.filter_by(email=user_fields["email"]).first()
+
+    if user:
+        return abort(400, description="Email already registered")
+
+    user = User()
+
+    user.email = user_fields["email"]
+    user.password = user_fields["password"]
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(user))
--- models/User.py
+++ models/User.py
@@ -1,7 +1,7 @@
 from main import db
 
 class User(db.Model):
-    __table__name = "users"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), nullable=False, unique=True)
